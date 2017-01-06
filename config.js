6232257360697fadea01595a62becc670d128642
menard_s <simon.menard@epitech.eu>
2017-01-06 18:51:21 +0800

    change / route

--- config.js
+++ config.js
@@ -32,7 +32,6 @@ var config = {
             selected_tags:Array
         },
         moment:{
-            _id: Number,
             owner_id:{type:String, required:true},
             image_url:String,
             title: {type:String, required:true},
